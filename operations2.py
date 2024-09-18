f1ccd6df1148bed99754aef59b069d9f214bad71
Mr GUI <mahboobicp@gmail.com>
2024-09-18 12:35:17 +0500

    Add Name Change Tab

--- operations2.py
+++ operations2.py
@@ -7,37 +7,92 @@ import mysql.connector
 from datetime import datetime
 from mysql.connector import Error
 import database as db
-gplotid=None
-gownerid=None
-gindid=None
+# Save record fn
+def save_record(indnameentery,naturecombo,statuscombo,modecombo,areaentery,dateentery):
+    if indnameentery.get() == "" or naturecombo.get() == "Select Nature" or statuscombo.get() == "Select Status" or modecombo.get() == "Select Mode" :
+        messagebox.showerror("Error","All fileds are required")
+    else:
+        try:
+            cur, con = db.database_connect()
+            cur.execute("use kpezdmc_version1")
+            plotid,owner_id = select_data()
+            if plotid == '':
+                messagebox.ERROR("Error","First select the plot from Tree")
+            else:
+                # Data Entery into Plot Table
+                ind_id = db.get_id("industries") # Get Plot tabel Id auto incrment by 1
+                # Define the SQL query to insert data
+                insert_query = """INSERT INTO industries (id,ind_name,ind_nature,ind_status,ind_mode,coverd_area,plot_id,created_at) 
+                                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+                # Get plot id and owner id from tree
+                # Cureent Date
+                current_date = datetime.now()
+
+                # Format the current date
+                formatted_date = current_date.strftime("%Y/%m/%d %H:%M:%S")  # Example format: 2024-09-03
+                print(plotid)
+                # Data to be inserted
+                data = (ind_id,indnameentery.get(),naturecombo.get(),statuscombo.get(),modecombo.get(),areaentery.get(),
+                        plotid,formatted_date)
+
+                # Execute the query
+                cur.execute(insert_query, data)
+        # End of entery to industry table
+
+        # Entery to industry ownership table
+                    # Data Entery into plot_ownership
+                ownership_id = db.get_id("industry_ownerships") # Get plot_Ownership Id Auto increment by 1
+                # Define the SQL query to insert data
+                insert_query = """INSERT INTO industry_ownerships (id,industry_id,owner_id,start_date,i0_status,created_at) 
+                                                        VALUES (%s, %s, %s,%s,%s,%s)"""
+
+                # Cureent Date
+                current_date = datetime.now()
+
+                # Format the current date
+                formatted_date = current_date.strftime("%Y/%m/%d %H:%M:%S")  # Example format: 2024-09-03
+                
+                # Data to be inserted
+                data = (ownership_id,ind_id,owner_id,dateentery.get(),"Established",formatted_date)
+
+                # Execute the query
+                cur.execute(insert_query, data) 
+                # Commit the transaction
+                con.commit()
+                clear_fields(indnameentery,naturecombo,statuscombo,modecombo,areaentery,dateentery)
+                treeview_data()
+        except Error as e:
+                messagebox.showerror("Error",f"Database error : {e}")
+        finally:
+                if con.is_connected():
+                    cur.close()
+                    con.close()
+                    print("MySQL connection is closed")
+        treeview_data()
+
 # Select Data from tree
-def select_data(event):
-    global gownerid,gplotid,gindid,oldname
-    row = []
-    index = treeview.selection()
-    print(f"Index is {index}")
-    content = treeview.item(index)
-    row = content['values']
-    print(row)
-    if row[7] == 'None':
-        gplotid = 0
-    else:
-        gplotid = row[7]
-    if row[8] == 'None':
-        gownerid = 0
-    else:
-        gownerid = row[8]
-    if row[9] == 'None':
-        gindid = 0
-    else:
-        gindid = row[9]
-    print(gplotid,gownerid,gindid)
-    oldname.set(f"Industry Name Is : {row[5]}")
-    # Print the values of the clicked row
+def select_data():
+    global treeview
+    try:
+        global plot_id,owner_id
+        plot_id = ''
+        owner_id = ''
+        index = treeview.selection()
+        content = treeview.item(index)
+        row = content['values']
+        plot_id = row[7]
+        owner_id = row[8]
+        print(f"Select Data : {row}")
+        print(plot_id,owner_id)  # Print the values of the clicked row
+        return plot_id,owner_id
+    except:
+        messagebox.showerror("Error","Select plot first")
 
 #Search Record
-# Function for Search Record
+
 def search_record(searchcombo,searchentry):
+    global treeview
     cond=searchcombo.get()
     value=f"'%{searchentry.get()}%'"
     if value == '':
@@ -51,25 +106,22 @@ def search_record(searchcombo,searchentry):
             cond = "i.ind_name"
         cur,con = db.database_connect()
         cur.execute("use kpezdmc_version1")
-        query =f"select p.plot_number,p.zone,p.Area,o.ownname,o.Mobile,i.ind_name,i.ind_nature,p.id,i.id,o.id from plots p join plot_ownership po on p.id = po.plot_id join ownertable o on o.id = po.owner_id left join industries i on i.plot_id = p.id where {cond} like {value};"
-        #print(query)
+        query =f"select p.plot_number,p.zone,p.Area,o.ownname,o.Mobile,i.ind_name,i.ind_nature from plots p join plot_ownership po on p.id = po.plot_id join ownertable o on o.id = po.owner_id left join industries i on i.plot_id = p.id where {cond} like {value};"
+        print(query)
         cur.execute(query)
         result = cur.fetchall()
         treeview.delete(*treeview.get_children())
         for record in result:
             treeview.insert('',ct.END,values=record)
-        plotid = result[0][7]
-        indid  = result[0][8]
-        ownid = result[0][9]
         print(result)
-        #print(plotid,indid,ownid)
-        #return plotid,indid,ownid
-        #plot_id,owner_id,indid=select_data()
-        #print(plot_id,owner_id,indid)
+ 
 # Clear Fields
-def clear_fields(paymentgeadcombo,amountentery,dateentery):
-    paymentgeadcombo.set("Select Head")
-    amountentery.delete(0,ct.END)
+def clear_fields(indnameentery,naturecombo,statuscombo,modecombo,areaentery,dateentery):
+    indnameentery.delete(0,ct.END)
+    naturecombo.set("Select Nature")
+    statuscombo.set("Select Status")
+    modecombo.set("Select Mode")
+    areaentery.delete(0,ct.END)
     from datetime import date
     dateentery.set_date(date.today())
 
@@ -78,7 +130,7 @@ def clear_fields(paymentgeadcombo,amountentery,dateentery):
 def treeview_data():
     cur, con = db.database_connect()
     cur.execute("use kpezdmc_version1")
-    query = """select p.plot_number,p.zone,p.Area,o.ownname,i.ind_status,i.ind_name,i.ind_nature,p.id,o.id,i.id
+    query = """select p.plot_number,p.zone,p.Area,o.ownname,o.Mobile,i.ind_name,i.ind_nature,p.id,o.id
                 from plots p
                 join
                 plot_ownership po
@@ -89,7 +141,7 @@ def treeview_data():
                 left join
                 industries i
                 on i.plot_id = p.id
-                order by i.created_at desc;"""
+                order by p.created_at desc;"""
     cur.execute(query)
     plot_record = cur.fetchall()
     treeview.delete(*treeview.get_children())
@@ -97,10 +149,7 @@ def treeview_data():
     for record in plot_record:
         treeview.insert('',ct.END,values=record)
 def operations(app):
-    gplotid = None
-    gownerid = None
-    gindid = None
-    global treeview,baltreeview,paytreeview,oldname
+    global treeview
     fontlable = ("Poppins",14)
     fontlmenu = ("Poppins",18,"bold")
     fontentry = ("Poppins",10,"bold")
@@ -110,17 +159,17 @@ def operations(app):
     backframe = ct.CTkFrame(operationframe,fg_color="#17202a")
     backframe.place(x=0,y=0)
     btnframe = ct.CTkFrame(operationframe,fg_color="#17202a")
-    btnframe.place(x=40,y=5)
+    btnframe.place(x=40,y=160)
     treeframe =ct.CTkFrame(operationframe,fg_color="#2c3e50",bg_color="#17202a",corner_radius=5,border_width=3,border_color="#85929e")
-    treeframe.place(x=0,y=60)
+    treeframe.place(x=0,y=25)
     tabsframe =ct.CTkFrame(operationframe,fg_color="#2c3e50",bg_color="#17202a",corner_radius=5,border_width=3,border_color="#85929e")
-    tabsframe.place(x=150,y=240)
+    tabsframe.place(x=0,y=200)
     photo_image = tkinter.PhotoImage(file=r"D:\Python\KPEZDMC\images\back.png")
     homebtn = ct.CTkButton(backframe,image=photo_image,text="",font=fontbtn,width=30,hover_color="#1b4f72",fg_color="#17202a",bg_color="#17202a",
                             height=20,cursor="hand2",command=lambda:operationframe.place_forget())
     homebtn.place(x=0,y=0)
     # Tree Frame start
-    plotdetaillable = ct.CTkLabel(treeframe,text="Plot / Industry Details",font=("Arial",14,"bold"),
+    plotdetaillable = ct.CTkLabel(treeframe,text="Plot Details",font=("Arial",14,"bold"),
                             text_color="#f8f9f9",bg_color="#808b96",width=850,height=20)
     plotdetaillable.pack()
     style = ttk.Style()
@@ -143,7 +192,7 @@ def operations(app):
     style.map("Treeview",
             background=[('selected', '#2980b9')],  # Background color when row is selected
             foreground=[('selected', 'white')]) # Text color when row is selected
-    cols = ("Plot #","Zone","Area","Owner","Status","indname","nature","Plot ID","Owner ID","Indid")
+    cols = ("Plot #","Zone","Area","Owner","Mobile","indname","nature","Plot ID","Owner ID")
     vsb = ttk.Scrollbar(treeframe, orient="vertical")
     h_scroll = ttk.Scrollbar(operationframe, orient="horizontal")
     treeview = ttk.Treeview(treeframe,columns = cols, show="headings",height=5,
@@ -158,8 +207,8 @@ def operations(app):
     treeview.heading ('Area', text="Area",anchor="center")
     treeview.column("Owner", width=140,anchor="center",stretch=False)
     treeview.heading ('Owner', text='Owner Name')
-    treeview.column("Status", width=120,anchor="center",stretch=False)
-    treeview.heading ('Status', text="Status")
+    treeview.column("Mobile", width=120,anchor="center",stretch=False)
+    treeview.heading ('Mobile', text="Mobile #")
     treeview.column("indname", width=130,anchor="center",stretch=False)
     treeview.heading ('indname', text="Industry Name",anchor="center")
     treeview.column("nature", width=110,anchor="center",stretch=False)
@@ -168,8 +217,6 @@ def operations(app):
     treeview.heading ('Plot ID', text="Plot ID")
     treeview.column("Owner ID", width=0,anchor="center",stretch=False)
     treeview.heading ('Owner ID', text="Owner ID")
-    treeview.column("Indid", width=0,anchor="center",stretch=False)
-    treeview.heading ('Indid', text="Indid")
     treeview_data()
     treeview.configure(yscrollcommand=vsb.set)
     # Add the horizontal scrollbar
@@ -182,71 +229,38 @@ def operations(app):
     vsb.pack(side=tkinter.RIGHT, fill=tkinter.Y)
     #vsb.grid(row=0,column=1,pady=0)
     #h_scroll.place(x=4,y=495,width=840)
-    #treeview.bind("Button-1>",select_data(event='TreeviewSelect'))
-    treeview.bind('<<TreeviewSelect>>', lambda event: select_data(event='TreeviewSelect'))
+    #treeview.bind("Button-1>",select_data)
+    #treeview.bind('<<TreeviewSelect>>', lambda event: select_data(event='TreeviewSelect'))
     #db.database_connect()
 
 
     #End of Tree Frame ###########################################
 
- 
-    # Strat of button Frame
-    searchlable = ct.CTkLabel(btnframe,text="Search By :",text_color="white")
-    searchlable.grid(row=0,column=0,padx=(50,0),pady=15)
-
-    searchcombo = ct.CTkComboBox(btnframe,font=fontentry,width=150,
-                                values=["Plot Number","Owner Name","Industry Name"],text_color="white",fg_color="#2c3e50",button_color="#707b7c",button_hover_color="#2471a3")
-    searchcombo.grid(row=0,column=1,padx=(30,0),pady=15)
-
-    searchentry = ct.CTkEntry(btnframe,placeholder_text="Search By",width=150,border_width=2,border_color="#99a3a4",
-                                fg_color="#2c3e50",text_color="White",placeholder_text_color="white")
-    searchentry.grid(row=0,column=2,padx=(30,0),pady=15)
-
-    searchbtn = ct.CTkButton(btnframe,text="Search",fg_color="#2c3e50",bg_color="#17202a",corner_radius=5,border_width=2,border_color="#85929e",width=150,command=lambda:search_record(searchcombo,searchentry))
-    searchbtn.grid(row=0,column=3,padx=(30,0),pady=15)
-
+   
     # Start of Tabs Frame
     # Create a CTkTabView
-    tab_view = ct.CTkTabview(tabsframe,fg_color="#2c3e50",bg_color="#17202a",text_color="white")
-    tab_view.pack(expand=False, padx=4, pady=(2,4))
-   # tab_view.place(x=10,y=20)
+    tab_view = ct.CTkTabview(tabsframe)
+    tab_view.pack(fill="both", expand=True, padx=5, pady=(2,5))
+
     # Add tabs to the TabView
     tab_view.add("Name Change")
     tab_view.add("Nature Change")
     tab_view.add("Director Change")
-    tab_view.add("NOC For WAPDA")
 
     # Set default active tab (optional)
     tab_view.set("Name Change")
+
     # Add content to Name Change
-    
-    label1 = ct.CTkLabel(tab_view.tab("Name Change"),text="Change of Name ", font=("Helvetica", 24,"bold"),text_color="white")
-    label1.place(x=110,y=2)
-    oldname = tkinter.StringVar()
-    oldname.set("Industry Name : ")
-    oldnamelable = ct.CTkLabel(tab_view.tab("Name Change"),textvariable=oldname,font=("Helvetica", 18),text_color="white")
-    oldnamelable.place(x=20,y=40)
-    newnamelable = ct.CTkLabel(tab_view.tab("Name Change"),text="Enter name     :   ",font=("Helvetica", 18),text_color="white")
-    newnamelable.place(x=20,y=80)
-    newname = tkinter.StringVar()
-    newnameentery = ct.CTkEntry(tab_view.tab("Name Change"),font=("Helvetica", 18),width=200,
-                                placeholder_text="Enter New Name",border_width=2,border_color="#17202a",
-                                fg_color="#154360",text_color="White",placeholder_text_color="white",textvariable=newname)
-    newnameentery.place(x=160,y=80)
-    newnameconflable = ct.CTkLabel(tab_view.tab("Name Change"),text="New Name Will be : ",font=("Helvetica", 18),text_color="white")
-    newnameconflable.place(x=20,y=120)
-    newnameconfirm = ct.CTkLabel(tab_view.tab("Name Change"),textvariable=newname,font=("Helvetica", 18,"bold"),text_color="white")
-    newnameconfirm.place(x=120,y=120)
-    savebtn = ct.CTkButton(tab_view.tab("Name Change"),text="Update Record",width=200,height=30,hover_color="darkgreen",cursor="hand2",
-                           fg_color="green",bg_color="#2c3e50",corner_radius=10,border_width=2,border_color="#17202a"
-                           )
-    savebtn.place(x=110,y=160)
-   
+    label1 = ct.CTkLabel(tab_view.tab("Name Change"), text="This is content for Name Change", font=("Helvetica", 16))
+    label1.pack(pady=150,padx=300)
+
     # Add content to Nature Change
-    label2 = ct.CTkLabel(tab_view.tab("Nature Change"),text="Nature Change", font=("Helvetica", 16))
+    label2 = ct.CTkLabel(tab_view.tab("Nature Change"), text="This is content for Nature Change", font=("Helvetica", 16))
     label2.pack(pady=20)
 
     # Add content to Director Change
     label3 = ct.CTkLabel(tab_view.tab("Director Change"), text="This is content for Director Change", font=("Helvetica", 16))
     label3.pack(pady=20)
 
+
+   
